42a7d0728ae261e57dbd79a38b45129442003888
Hamad Alsherooqi <202001187@student.polytechnic.bh>
2023-11-15 13:06:00 +0300

    Initial Commit

---
+++ MinSimplyGitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinSimplyGitTutorial
+//
+//  Created by Hamad Tareq Mohamed Saleh Abdulla Alsherooqi on 15/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let mwssage = "Hello Git"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(mwssage)
+        
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
